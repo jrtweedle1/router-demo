3d35817fd26a9f4dafdbc107bd98cd76fbfba356
jrtweedle1 <tweedle@google.com>
2024-04-08 12:08:29 -0500

    Finish configuring the loader on the route.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Root, {
+	loader as rootLoader,
+	action as rootAction,
+} from "./routes/root";
 import "./index.css";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 // First route, "root" route
 const router = createBrowserRouter([
@@ -13,8 +15,9 @@ const router = createBrowserRouter([
 		path: "/",
 		element: <Root />,
 		errorElement: <ErrorPage />,
-        loader: rootLoader,
-        action: rootAction,
+		loader: rootLoader,
+		action: rootAction,
+        loader: contactLoader,
 		children: [
 			{
 				path: "contacts/:contactId",
